64ed11fb77c4d4b7ebb0f128c56cf2b0bfd7ec35
AlloKuz <a.o.kuzmin.rus@gmail.com>
2024-08-23 16:41:54 +0300

    add format, step 2

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,6 +7,11 @@ def main():
                 description="Compares two configuration files and shows a difference.")
     parser.add_argument("first_file", type=str)
     parser.add_argument("second_file", type=str)
+    parser.add_argument("-f", "--format",
+                        metavar="FORMAT",
+                        type=str,
+                        choices=["plain", "json"],
+                        help="set format of output")
 
     args = parser.parse_args()
 
