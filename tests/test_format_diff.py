c53dd36b26bd31c7690a4488d3bdc4c5c56bf53f
AlloKuz <a.o.kuzmin.rus@gmail.com>
2024-08-23 21:01:13 +0300

    add fixtures for tests

--- tests/test_format_diff.py
+++ tests/test_format_diff.py
@@ -1,6 +1,22 @@
 from gendiff.utils.format_diff import *
 
 import pytest
+import json
+
+
+@pytest.fixture
+def test_sort_diff_data():
+    with open("tests/fixtures/test_sort_diff_data.json") as f:
+        data = json.load(f)
+    return data
+
+
+@pytest.fixture
+def test_sort_diff_data_sorted():
+    with open("tests/fixtures/test_sort_diff_data_sorted.json") as f:
+        data = json.load(f)
+    return data
+
 
 def test_get_item_name():
     assert get_item_name("test", prefix="+") == "+ test"
@@ -9,25 +25,9 @@ def test_get_item_name():
     assert get_item_name("", prefix="+") == "+ "
 
 
-def test_sort_diff():
-    data = {
-        "+ one": 2,
-        "  two": 1,
-        "+ three": 2,
-        "- four": 2,
-        "  five": 3,
-        "- three": 3,
-        "- one": 1,
-    }
-    result = {
-        "- four": 2,
-        "  five": 3,
-        "- one": 1,
-        "+ one": 2,
-        "  two": 1,
-        "- three": 3,
-        "+ three": 2,
-    }
+def test_sort_diff(test_sort_diff_data, test_sort_diff_data_sorted):
+    data = test_sort_diff_data
+    result = test_sort_diff_data_sorted
     assert sort_diff(data) == result
     assert sort_diff({}) == {}
 
