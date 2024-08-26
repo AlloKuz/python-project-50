62fdf14c1e398976f63cc3f67760c4b07e1baef2
AlloKuz <a.o.kuzmin.rus@gmail.com>
2024-08-26 15:05:25 +0300

    fix plain

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -1,16 +1,24 @@
-import json
-
-
 def _get_name(name, path):
     if not path:
         return name
     return f"{path}.{name}"
 
 
-def _replace_complex_with_text(value):
+JSON_WORDS = {
+    True: "true",
+    False: "false",
+    None: "null"
+}
+
+
+def _prepare_value(value):
     if isinstance(value, (dict, list)):
         return "[complex value]"
-    return json.dumps(value)
+    if isinstance(value, bool) or value is None:
+        return JSON_WORDS[value]
+    if isinstance(value, (int, float)):
+        return value
+    return f"'{value}'"
 
 
 def _format_diff(data, *, path=''):
@@ -33,14 +41,14 @@ def _format_diff(data, *, path=''):
 
             case "added":
                 item_name = _get_name(item["name"], path)
-                value = _replace_complex_with_text(item["value"])
+                value = _prepare_value(item["value"])
                 item_string = templates_dict["added"].format(name=item_name,
                                                              value=value)
                 result.append(item_string)
 
             case "removed":
                 item_name = _get_name(item["name"], path)
-                value = _replace_complex_with_text(item["value"])
+                value = _prepare_value(item["value"])
                 temp = templates_dict["removed"]
                 item_string = temp.format(name=item_name,
                                           value=value)
@@ -48,8 +56,8 @@ def _format_diff(data, *, path=''):
 
             case "changed":
                 item_name = _get_name(item["name"], path)
-                old_value = _replace_complex_with_text(item["value"][0])
-                new_value = _replace_complex_with_text(item["value"][1])
+                old_value = _prepare_value(item["value"][0])
+                new_value = _prepare_value(item["value"][1])
                 temp = templates_dict["updated"]
                 item_string = temp.format(name=item_name,
                                           old_value=old_value,
