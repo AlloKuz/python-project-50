113420006f2528421c0387360d81a35d6e3a7345
AlloKuz <a.o.kuzmin.rus@gmail.com>
2024-08-26 16:23:21 +0300

    updt make_diff

--- gendiff/make_diff.py
+++ gendiff/make_diff.py
@@ -4,52 +4,46 @@ def _prepare_data_item(name, state, value, is_nested=False):
             "children" if is_nested else "value": value}
 
 
-def _is_nested(key, data1, data2):
-    if all(isinstance(x.get(key), dict) for x in (data1, data2)):
-        return True
-    return False
-
-
 def make_diff(data1, data2):
+
     result_data = []
 
     added_keys = set(data2) - set(data1)
 
-    all_keys = set(data1) | set(data2)
+    removed_keys = set(data1) - set(data2)
 
-    nested_keys = set()
+    all_keys = set(data1) | set(data2)
 
     for key in all_keys:
         # check if nested
-        if _is_nested(key, data1, data2):
-            nested_keys.add(key)
-
-        if key in all_keys - nested_keys:
-            data_by_status = {
-                "added": data2.get(key, None),
-                "removed": data1.get(key, None),
-                "notchanged": data1.get(key, None),
-                "changed": [data1.get(key, None), data2.get(key, None)]}
-
-            current_status = None
-            if key in set(data1) ^ set(data2):
-                current_status = "added" if key in added_keys else "removed"
-            else:
-                current_status = ("notchanged"
-                                  if (data1[key] == data2[key])
-                                  else "changed")
-
-            new_data = _prepare_data_item(key,
-                                          current_status,
-                                          data_by_status[current_status])
-
-            result_data.append(new_data)
-
-        if key in nested_keys:
+        if all(isinstance(x.get(key), dict) for x in (data1, data2)):
             new_data = make_diff(data1[key], data2[key])
             result_data.append(_prepare_data_item(key,
                                                   "nested",
                                                   new_data,
                                                   is_nested=True))
+            continue
+
+        data_by_status = {
+            "added": data2.get(key, None),
+            "removed": data1.get(key, None),
+            "notchanged": data1.get(key, None),
+            "changed": [data1.get(key, None), data2.get(key, None)]}
+
+        current_status = None
+        if key in added_keys:
+            current_status = "added"
+        elif key in removed_keys:
+            current_status = "removed"
+        else:
+            current_status = ("notchanged"
+                              if (data1[key] == data2[key])
+                              else "changed")
+
+        new_data = _prepare_data_item(key,
+                                      current_status,
+                                      data_by_status[current_status])
+
+        result_data.append(new_data)
 
     return result_data
