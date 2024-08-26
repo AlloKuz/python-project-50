ab7684de69dbfd3d1432260945b7678167d2c157
AlloKuz <a.o.kuzmin.rus@gmail.com>
2024-08-26 16:01:48 +0300

    fix

--- gendiff/formatters/json.py
+++ gendiff/formatters/json.py
@@ -1,10 +1,9 @@
 from .utils.make_json_diff import make_json_diff
 from .stylish import _format_stylish
 
+import json
+
 
 def format(data, raw=False):
     prepared_data = make_json_diff(data)
-    return prepared_data if not raw else _format_stylish(prepared_data,
-                                                     indent_symbol="",
-                                                     indent_size=0,
-                                                     shift_size=0)
+    return prepared_data if not raw else json.dumps(prepared_data)
