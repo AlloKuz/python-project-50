eac222f93ec0ba5c5e4deaf2b6b11e589f2864ec
AlloKuz <a.o.kuzmin.rus@gmail.com>
2024-08-27 15:04:08 +0300

    fix

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -8,21 +8,15 @@ def parse(data, format="json"):
         return json.loads(data)
     elif format in ("yml", "yaml"):
         return yaml.safe_load(data)
-    else:
-        raise ValueError("Wrong format!")
-    return None
+    raise ValueError("Wrong format! Needs: json/yaml file!")
 
 
 def get_content(filename):
 
-    ext = os.path.splitext(filename)[-1].replace(".", "")
-
-    if not filename or not os.path.isfile(filename):
-        raise ValueError(f"File '{filename}' does not exists!")
-
     with open(filename) as f:
-        content = f.read()
 
-    data = parse(content, ext)
+        _, ext = os.path.splitext(filename)
+
+        data = parse(f.read(), ext[1:])
 
     return data
