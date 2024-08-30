d2b35faf7cce26ae5d789fdc1a738b69b627e90b
AlloKuz <a.o.kuzmin.rus@gmail.com>
2024-08-30 13:45:09 +0300

    relocate formatter

--- gendiff/formatters/__init__.py
+++ gendiff/formatters/__init__.py
@@ -1,5 +1,5 @@
 from gendiff.formatters.json import json_formatter
-from gendiff.formatters.plain import plain_formatter,
+from gendiff.formatters.plain import plain_formatter
 from gendiff.formatters.stylish import stylish_formatter
 
 
