d227979c962715615e10e2ba23cd7b7dafe1c316
AlloKuz <a.o.kuzmin.rus@gmail.com>
2024-08-30 13:51:36 +0300

    relocate formatter

--- gendiff/formatters/__init__.py
+++ gendiff/formatters/__init__.py
@@ -1,6 +1,6 @@
-from json import json_formatter
-from plain import plain_formatter
-from stylish import stylish_formatter
+from .json import format as json_formatter
+from .plain import format as plain_formatter
+from .stylish import format as stylish_formatter
 
 
 def format_data(data, output_type="json"):
@@ -14,3 +14,11 @@ def format_data(data, output_type="json"):
             return plain_formatter(data)
         case _:
             return stylish_formatter(data)
+
+
+__all__ = (
+    "json_formatter",
+    "plain_formatter",
+    "stylish_formatter",
+    "format_data",
+)
