efa4eacab55bd6504e0affc0f9728188f874de62
AlloKuz <a.o.kuzmin.rus@gmail.com>
2024-08-30 14:21:10 +0300

    add tests after review (2)

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,4 +1,6 @@
 from gendiff import generate_diff
+from parser import parse
+from gendiff.formatters.plain import transform_to_str
 
 import pytest
 
@@ -28,3 +30,19 @@ FIXTURES_PATH = "tests/fixtures"
 def test_gen_diff_output(file1, file2, expected_path, output_type):
     with open(expected_path) as f:
         assert generate_diff(file1, file2, output_type=output_type) == f.read()
+
+
+def test_exception_type_of_file():
+    assert parse(
+        'tests/fixtures/json_difference_for_test_data.txt'
+    ) == {'Exception': 'file has wrong format'}
+
+
+def test_exception_format():
+    for value in ['50', [True], 'text',]:
+        assert transform_to_str(value) == f"'{str(value)}'"
+    assert transform_to_str(True) == 'true'
+    assert transform_to_str(False) == 'false'
+    assert transform_to_str(None) == 'null'
+    assert transform_to_str(50) == 50
+    assert transform_to_str('[complex value]') == '[complex value]'
