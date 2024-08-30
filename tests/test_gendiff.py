4b5466f3c634794150724fb0c3e226e60e38bae1
AlloKuz <a.o.kuzmin.rus@gmail.com>
2024-08-30 15:16:45 +0300

    test format file

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,6 +1,4 @@
 from gendiff import generate_diff
-from gendiff.parser import parse
-from gendiff.formatters.plain import transform_to_str
 
 import pytest
 
@@ -31,9 +29,3 @@ FIXTURES_PATH = "tests/fixtures"
 def test_gen_diff_output(file1, file2, expected_path, output_type):
     with open(expected_path) as f:
         assert generate_diff(file1, file2, output_type=output_type) == f.read()
-
-
-def test_exception_type_of_file():
-    assert parse(
-        '{FIXTURES_PATH}/file1_2_json.txt'
-    ) == {"Wrong format! Needs: json/yaml file!"}
